6ebf34cb706fc2042c68c31a20598053c719bde2
Johnson Kanyi <125729582+kanyijohn@users.noreply.github.com>
2024-01-14 11:25:08 +0300

    App.js
    App.js

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const currDate = new Date();
+
   return (
-     <div className="App">
-       <h1>Hello World!</h1>
+    <div>
+      <h1>Hello, world! I am John.</h1>
+      <h2>It is {currDate.toLocaleDateString()} and the time now is {currDate.toLocaleTimeString()}.</h2>
     </div>
-      );
-    }
-    
-    export default App;
+  );
+}
+
+export default App;
   
 
